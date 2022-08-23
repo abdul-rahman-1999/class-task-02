ad939667fb8ce714ee4ec608f62739456a300cf5
abdul-rahman-1999 <97880873+abdul-rahman-1999@users.noreply.github.com>
2022-08-23 05:34:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Task
+// find average with one array method only
+
+
+
+// find the topper's name using one array method
+
+const findTopper = scores.find((n) => n.marks > 90);
+console.log(findTopper.name);
